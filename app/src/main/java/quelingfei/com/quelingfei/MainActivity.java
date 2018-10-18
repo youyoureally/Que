8ffeed2b91e50cf37e2e02e3395f7dbd887906e0
youyoureally <76880784@qq.com>
2018-10-18 11:18:22 +0800

    提交

--- app/src/main/java/quelingfei/com/quelingfei/MainActivity.java
+++ app/src/main/java/quelingfei/com/quelingfei/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
     private TextView mTextMessage;
 
@@ -26,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
-
             }
             return false;
         }
